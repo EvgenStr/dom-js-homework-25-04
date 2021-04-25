afa0c76477acb39ef92da9faf469e83be8d76265
EvgenStr <juve2910@gmail.com>
2021-04-25 12:28:20 +0300

    init

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+/*1.1 Создать кнопку, у которой в атрибуте data-counter записано число.
+ При нажатии на кнопку число должно уменьшаться. Когда счетчик доходит до 0,
+  обработчик события должен удалиться.
+ Значение счетчика должно отображаться в innerText кнопки.*/
+
+const btnCounter = document.querySelector("#btn-counter");
+
+const decrease = ({ target }) => {
+
+  target.innerText = --target.dataset.counter;
+}
